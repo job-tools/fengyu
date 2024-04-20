58a2510b2ddb1b75baef6fcba2f451458afa7ced
mnisting <2427229963@qq.com>
2024-04-20 16:53:55 +0800

    feat: 登录注册页面初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,30 +3,49 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // 登录方式选择页
     {
       path: '/login',
-      component: () => import('@/views/login/login.vue')
+      component: () => import('@/views/login/index.vue')
+    },
+    // 账号密码登录
+    {
+      path: '/login/account',
+      component: () => import('@/views/login/account.vue')
+    },
+    // 手机验证码登录
+    {
+      path: '/login/sms',
+      component: () => import('@/views/login/sms.vue')
+    },
+    // 注册
+    {
+      path: '/login/registry',
+      component: () => import('@/views/login/registry.vue')
+    },
+    // 个人中心
+    {
+      path: '/personalCenter',
+      component: () => import('@/views/personalCenter/index.vue')
     },
 
     {
       path: '/',
       name: 'layout',
       component: () => import('@/layout/index.vue'),
-
       children: [
+        // {
+        //   path: 'role',
+        //   component: () => import('@/views/role/index.vue'),
+        //   meta: { name1: '角色管理', name2: '角色列表' }
+        // },
         {
-          path: '/',
-          component: () => import('@/views/Index/Index.vue'),
-          meta: { name1: '首页', name2: '首页' }
-        },
-
-        {
-          path: 'publishingLssues',
+          path: 'question/create',
           component: () => import('@/views/publishingLssues/publishingLssues.vue'),
           meta: { name: '发布问题' }
         },
         {
-          path: 'problemList',
+          path: 'question',
           component: () => import('@/views/problemList/problemList.vue'),
           meta: { name: '问题列表' }
         }
